98eae6fd770b7be2c26aeedbbd953492ed044b87
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-05-30 16:58:39 +0900

    ref dom 접근

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import EventPractice from './EventPractice';
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
 
-const App = () => {
-  return <EventPractice />;
-};
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox = ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()} >맨 밑으로</button>
+      </div>
+    );
+  }
+}
+
+
 
 
 export default App;
