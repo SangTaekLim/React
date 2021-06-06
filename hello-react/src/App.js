1d5ed4abff700e55be6c54efd52aef195bba72d1
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-06-06 15:01:12 +0900

    컴포넌트 반복

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => this.scrollBox = ref} />
-        <button onClick={() => this.scrollBox.scrollToBottom()} >맨 밑으로</button>
+        <IterationSample />
       </div>
     );
   }
