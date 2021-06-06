c9a8ca52833c21a343af447d948851ac143da6d7
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-06-06 16:13:02 +0900

    컴포넌트 라이프사이클

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    })
+  }
   render() {
     return (
       <div>
-        <IterationSample />
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
---
+++ hello-react/src/ErrorBoundary.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+    state = {
+        error: false
+    }
+    componentDidCatch(error, info) {
+        this.setState({
+            error: true
+        });
+        console.log({ error, info });
+    }
+    render() {
+        if (this.state.error) return <div>에러발생</div>;
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
+
