ebea2abed29b3a9e896dcbd62262a0ffada8b9f9
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-06-13 14:59:53 +0900

    Hook

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Info from './Info';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
+
 
   return (
-    <div>
-      <button onClick={() => {
-        setVisible(!visible);
-      }}>{visible ? '숨기기' : '보이기'}</button>
-      <hr />
-      {visible && <Info />}
-    </div>
+    <Info />
   );
 }
 
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,37 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+import useInputs from './useInputs';
+
 
 const Info = () => {
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
-    useEffect(() => {
-        console.log('effect');
-        console.log(name);
-        return () => {
-            console.log('cleanup');
-            console.log(name);
-        };
-    }, [name]);
+    const [state, onChange] = useInputs({
+        name: '',
+        nickname: ''
+    });
+    const { name, nickname } = state;
 
-    const onChangeName = e => {
-        setName(e.target.value);
-    };
-
-    const onChangeNickname = e => {
-        setNickname(e.target.value);
-    };
 
     return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName} />
-                <input value={nickname} onChange={onChangeNickname} />
+                <input name="name" value={name} onChange={onChange} />
+                <input name="nickname" value={nickname} onChange={onChange} />
             </div>
             <div>
                 <div>
-                    <b>이름 : </b> {name}
+                    <b>이름:</b> {name}
                 </div>
                 <div>
-                    <b>닉네임 : </b> {nickname}
+                    <b>닉네임 :</b>
+                    {nickname}
                 </div>
             </div>
         </div>
