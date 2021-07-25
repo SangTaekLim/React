3dc7f0443c7a2ea22a158d31ec992d790b072b9a
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-07-25 14:47:21 +0900

    context api

---
+++ context-tutorial/src/components/ColorBox.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ColorConsumer } from '../contexts/color';
+
+const ColorBox = () => {
+    return (
+        <ColorConsumer>
+            {({ state }) => (
+                <>
+                    <div
+                        style={{
+                            width: '64px',
+                            height: '64px',
+                            background: state.color
+                        }}
+                    />
+                    <div
+                        style={{
+                            width: '32px',
+                            height: '32px',
+                            background: state.subcolor
+                        }}
+                    />
+                </>
+            )}
+        </ColorConsumer>
+    );
+}
+
+export default ColorBox;
