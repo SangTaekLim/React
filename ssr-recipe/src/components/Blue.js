49f65af12716eb79f9cf6db7cad3edd045cff0ee
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-08-08 15:55:32 +0900

    Commit before eject

---
+++ ssr-recipe/src/components/Blue.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Blue.css';
+
+const Blue = () => {
+    return <div className="Blue">Blue</div>
+};
+
+export default Blue;
+
+
---
+++ ssr-recipe/src/components/Red.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Red.css';
+
+
+const Red = () => {
+    return <div className="Red">Red</div>
+};
+
+export default Red;
+
