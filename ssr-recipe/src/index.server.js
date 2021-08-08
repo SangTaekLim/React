b2efb893e5883bda67ef135240f2edbe7b5d3600
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-08-08 17:53:17 +0900

    서버사이드 랜더링

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter, staticRouter } from 'react-router-dom';
+import App from './App';
 
-const html = ReactDOMServer.renderToString(
-  <div>Hello Server Side Rendering</div>,
-);
+const app = express();
 
-console.log(html);
+const serverRender = (req, res, next) => {
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx);
+  res.send(root);
+};
+
+const serve = express.static(path.resolve('./build'), {
+  index: false,
+});
+
+app.use(serve);
+
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log('Running on http://localhost:5000');
+});
