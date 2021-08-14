6da2b5a437007b315d44386b45237aa033e6d4ba
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-08-14 13:16:53 +0900

    koa 617

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,10 +2,6 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
-
 app.listen(4000, () => {
   console.log('Listing to port 4000');
 });
